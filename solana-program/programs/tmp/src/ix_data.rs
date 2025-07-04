c0fa602e2f4fb443749135423c8462d1fcef3f8a
kanihalos63 <114111624+kanihalos63@users.noreply.github.com>
2025-07-04 15:33:48 +0300

    Delete solana-program directory

--- solana-program/programs/tmp/src/ix_data.rs
+++
@@ -1,7 +0,0 @@
-use anchor_lang::prelude::*;
-
-#[derive(AnchorSerialize, AnchorDeserialize, Clone, Debug)]
-pub struct SwapData {
-    pub amount_in: u64,
-    pub minimum_amount_out: u64,
-} 
--- solana-program/programs/tmp/src/state/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod arbitrage_state;
-pub mod swap_state;
-
-pub use arbitrage_state::*;
-pub use swap_state::RaydiumSwapState;
